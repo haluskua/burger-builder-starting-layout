7a01b0b8c530cd4ffb0d1e7cc9961e396a6de569
haluskua <haluskua.one@gmail.com>
2020-05-10 11:14:17 +1200

    added Order button with logic of updated ingredients function passed through virtual DOM

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,8 +27,23 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0, 
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchaseable: false
   }
+//function
+  updatePurchaseState (ingredients) {
+
+    const sum = Object.keys(ingredients)
+    .map(igKey => {
+      return ingredients[igKey];
+    })
+    .reduce((sum, el) => {
+      return sum + el;
+    }, 0);
+    this.setState({purchaseable: sum > 0});
+  }
+
+
   addIngredientHandeler = (type) => {
     const oldCount = this.state.ingredients[type];
     const updatedCount = oldCount + 1;
@@ -40,7 +55,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
-    
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -52,12 +67,13 @@ class BurgerBuilder extends Component {
     const updatedIngredients = {
       ...this.state.ingredients
     }
+
     updatedIngredients[type] = updatedCount;
     const priceDeduction = INGREDIENT_PRICES[type];
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceDeduction;
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
-
+    this.updatePurchaseState(updatedIngredients);
   }
 
 
@@ -78,6 +94,7 @@ class BurgerBuilder extends Component {
         ingredientRemoved={this.removeIngredientHandler}
         indgredientAdded={this.addIngredientHandeler} 
         disabled={disabledInfo}
+        purchaseable={this.state.purchaseable}
         price={this.state.totalPrice}/>
       </Auxillary>
     );
