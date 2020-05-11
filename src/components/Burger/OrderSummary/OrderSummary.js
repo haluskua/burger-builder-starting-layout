4c5544b30af66532a36ca7b4af19f66a19bbcdc9
haluskua <haluskua.one@gmail.com>
2020-05-11 16:53:49 +1200

    added total price to Order Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,6 +3,7 @@ import Auxillary from "../../../hoc/Auxillary";
 import Button from '../../UI/Button/Button';
 
 
+
 const OrderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
     .map(igKey => {
@@ -17,6 +18,7 @@ const OrderSummary = (props) => {
       <h3>Your Order</h3>
       <p>A deliscious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
       <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
